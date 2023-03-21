6d080830f8aef85946160f2d93b6a7d8611d08d0
tunix78 <svengauggel@gmail.com>
2023-03-21 09:56:09 +0000

    Adding base directory

--- scripts/pull_docs_folders.sh
+++ scripts/pull_docs_folders.sh
@@ -2,17 +2,19 @@
 
 # we need to get 3 input parameters, the directory that we can find all the module docs in, the source dir for the master docs and the target path to put all the docs into
 if [[ $# != 3 ]]; then
-    echo "Usage: $0 <src_modules_dir> <mstr_module_docs_dir> <tgt_module_docs_dir>"
-    echo "Example: $0 ~/mytemp/all_modules ~/terraform/moduleDocs /tmp/module_docs"
+    echo "Usage: $0 <base_dir> <src_modules_dir> <mstr_module_docs_dir> <tgt_module_docs_dir>"
+    echo "Example: $0 /tmp src_modules mstr_moduleDocs tgt_module_docs"
     exit 1
 fi
 
-SRC_MODULES_DIR=$1
-MSTR_MODULE_DOCS_DIR=$2
-TGT_MODULE_DOCS_DIR=$3
+BASE_DIR=$1
+SRC_MODULES_DIR=$1/$2
+MSTR_MODULE_DOCS_DIR=$1/$3
+TGT_MODULE_DOCS_DIR=$1/$4
 MODULES_TO_CLONE=stackrModule
+echo "Using ${BASE_DIR} as the base directory"
 echo "Using ${SRC_MODULES_DIR} as the source of all our module documentation"
-echo "Using ${MSTR_MODULE_DOCS_DIR} as the source of the master documentation"
+echo "Using ${MSTR_MODULE_DOCS_DIR} as the directory of the master documentation"
 echo "Using ${TGT_MODULE_DOCS_DIR} as the target for all our compiled documentation"
 
 # Create the source modules dir in case it doesn't exist
